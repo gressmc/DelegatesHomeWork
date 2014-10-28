2210f02b0089ea03db9470ca6bb4a07255d7edf9
gressmc <gressmc@iMac.local>
2014-10-28 21:29:10 +0300

    Initial Commit

---
+++ DelegatesHomeWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegatesHomeWork
+//
+//  Created by gressmc on 28/10/14.
+//  Copyright (c) 2014 gressmc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
